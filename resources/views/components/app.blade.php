667748d94e193d18cda39f7f58b2eb07581f0489
joserodny <josesibugan@gmail.com>
2022-06-20 20:15:21 +0800

    add meta tags

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://metatags.io/">
-    <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
-    <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="og:url" content="http://techjobads.herokuapp.com/">
+    <meta property="og:title" content="Looking for a job ?">
+    <meta property="og:description" content="Help shape the future by joining one of the fastest growing technology startups.">
+    <meta property="og:image" content="https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @stack('style')
